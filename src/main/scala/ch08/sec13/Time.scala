dbeb74abc924779e9a390e5e8571c78ef1ca3441
sebastian.gherghesanu <sebastian.gherghesanu@tribalworldwide.co.uk>
2019-02-07 18:42:51 +0200

    refactoring project structure
    refactoring project structure

--- src/main/scala/ch08/sec13/Time.scala
+++ src/main/scala/ch08/sec13/Time.scala
@@ -1,13 +1,13 @@
 package ch08.sec13
 
 class MilTime private(val time: Int) extends AnyVal {
-  def minutes = time % 100
-  def hours = time / 100
+  def minutes: Int = time % 100
+  def hours: Int = time / 100
   override def toString = f"$time"
 }
 
 object MilTime {
-  def apply(t: Int) =
+  def apply(t: Int): MilTime =
     if (0 <= t && t < 2400 && t % 100 < 60) new MilTime(t)
     else throw new IllegalArgumentException
 }
@@ -16,3 +16,9 @@ object Main extends App {
   val lunch = MilTime(1230)
   println(lunch)
 }
+
+
+
+
+
+
